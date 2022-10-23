07ada48cd77eb79498d0e58c49adaaa52217eac8
thephotogenicbug <navinkumarachari@gmail.com>
2022-10-23 22:16:59 +0530

    finalize enterData and Transfer data to SecondActivity

--- app/src/main/java/com/naveen/intent/MainActivity.kt
+++ app/src/main/java/com/naveen/intent/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.naveen.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var name : EditText
+    lateinit var age : EditText
+    lateinit var send : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.editTextName)
+        age = findViewById(R.id.editTextNumber)
+        send = findViewById(R.id.button)
+
+        send.setOnClickListener {
+
+            var username : String = name.text.toString()
+            var userage : Int = age.text.toString().toInt()
+
+            var intent = Intent(this@MainActivity, SecondActivity::class.java)
+            intent.putExtra("username", username)
+            intent.putExtra("userage", userage)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/naveen/intent/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.naveen.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+
+    lateinit var result : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        result = findViewById(R.id.textResult)
+
+       var userName : String =  intent.getStringExtra("username").toString()
+       var userAge : Int = intent.getIntExtra("userage", 0 )
+
+       result.text = "Your name is $userName  and your age is $userAge"
+    }
+}
